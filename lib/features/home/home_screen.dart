d5c3cab73053349ef8a885182f6b410ef1f95664
jinki <jkhsky121@gmail.com>
2023-07-03 17:18:52 +0900

    무한 스크롤

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:caffe_app/constants/gaps.dart';
 import 'package:caffe_app/constants/sizes.dart';
 import 'package:caffe_app/features/home/search_screen.dart';
+import 'package:caffe_app/features/home/setting_bar_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -19,6 +20,31 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _itemCount = 10;
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollListener() {
+    if (_scrollController.position.pixels ==
+        _scrollController.position.maxScrollExtent) {
+      setState(() {
+        _itemCount += 10;
+      });
+    }
+  }
+
   void _onTabBarTap(int index) {
     FocusScope.of(context).unfocus();
   }
@@ -27,6 +53,15 @@ class _HomeScreenState extends State<HomeScreen> {
     await showCupertinoModalPopup(
       context: context,
       builder: (context) => const SearchScreen(),
+    );
+  }
+
+  void _onSettingBarTap(BuildContext context) async {
+    await showDialog(
+      useSafeArea: false,
+      barrierColor: Colors.transparent,
+      context: context,
+      builder: (context) => const SettingBarScreen(),
     );
   }
 
@@ -73,9 +108,12 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
                 Gaps.h16,
-                const SizedBox(
-                  child: FaIcon(
-                    FontAwesomeIcons.bars,
+                GestureDetector(
+                  onTap: () => _onSettingBarTap(context),
+                  child: const SizedBox(
+                    child: FaIcon(
+                      FontAwesomeIcons.bars,
+                    ),
                   ),
                 ),
               ],
@@ -102,17 +140,18 @@ class _HomeScreenState extends State<HomeScreen> {
         body: TabBarView(
           children: [
             GridView.builder(
-              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
               padding: const EdgeInsets.symmetric(
                 horizontal: Sizes.size20,
                 vertical: Sizes.size16,
               ),
-              itemCount: 20,
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 1,
                 mainAxisSpacing: Sizes.size10,
                 childAspectRatio: 1,
               ),
+              findChildIndexCallback: (key) => null,
+              itemCount: _itemCount,
+              controller: _scrollController,
               itemBuilder: (context, index) => Column(
                 children: [
                   Container(
---
+++ lib/features/home/setting_bar_screen.dart
@@ -0,0 +1,113 @@
+import 'package:caffe_app/constants/gaps.dart';
+import 'package:caffe_app/constants/sizes.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class SettingBarScreen extends StatelessWidget {
+  const SettingBarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white.withOpacity(0.3),
+        body: Align(
+          alignment: Alignment.centerRight,
+          child: Container(
+            width: MediaQuery.of(context).size.width / 1.7,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: Sizes.size20,
+                vertical: Sizes.size20,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    onTap: () => Navigator.of(context).pop(),
+                    child: const FaIcon(
+                      FontAwesomeIcons.circleXmark,
+                    ),
+                  ),
+                  Gaps.v80,
+                  const Text(
+                    '닉네임',
+                    style: TextStyle(
+                      fontSize: Sizes.size24,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Gaps.v10,
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: 20,
+                        height: 20,
+                        child: Image.asset(
+                          'assets/icons/kakao.png',
+                        ),
+                      ),
+                      Gaps.h10,
+                      const Expanded(
+                        child: Text(
+                          'email@address.com',
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: Sizes.size16,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Gaps.v20,
+                  Container(
+                    width: double.infinity,
+                    height: 1.5,
+                    color: Colors.grey[400],
+                  ),
+                  Gaps.v20,
+                  const DefaultTextStyle(
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: Sizes.size16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            FaIcon(
+                              FontAwesomeIcons.heart,
+                              size: Sizes.size20,
+                            ),
+                            Gaps.h14,
+                            Text('위시리스트'),
+                          ],
+                        ),
+                        Gaps.v20,
+                        Row(
+                          children: [
+                            FaIcon(
+                              FontAwesomeIcons.gear,
+                              size: Sizes.size20,
+                            ),
+                            Gaps.h14,
+                            Text('설정'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
