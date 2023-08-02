99557a22dfee9bc7dc58fd93369fb3559ff1b60d
jinki <jkhsky121@gmail.com>
2023-08-02 15:40:10 +0900

    로그인 여부에 따라 시작 스크린 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:caffe_app/features/authentication/repos/authentication_repo.dart';
 import 'package:caffe_app/features/authentication/view_models/agree_vm.dart';
 import 'package:caffe_app/features/authentication/views/first_screen.dart';
 import 'package:caffe_app/features/home/views/home_screen.dart';
@@ -6,7 +7,8 @@ import 'package:caffe_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:provider/provider.dart' as provider;
 
 import 'data/cafe_repository.dart';
 
@@ -30,28 +32,32 @@ void main() async {
     ),
   );
 
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (context) => AgreeViewModel()),
-      ChangeNotifierProvider(
-          create: (context) => ItemViewModel(
-                repository: CafeRepository(),
-              )),
-    ],
-    child: const CaffeApp(),
+  runApp(ProviderScope(
+    child: provider.MultiProvider(
+      providers: [
+        provider.ChangeNotifierProvider(create: (context) => AgreeViewModel()),
+        provider.ChangeNotifierProvider(
+            create: (context) => ItemViewModel(
+                  repository: CafeRepository(),
+                )),
+      ],
+      child: const CaffeApp(),
+    ),
   ));
 }
 
-class CaffeApp extends StatelessWidget {
+class CaffeApp extends ConsumerWidget {
   const CaffeApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final auth = ref.watch(authRepo);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Caffe App',
       themeMode: ThemeMode.light,
-      home: const HomeScreen(),
+      home: auth.isLoggedIn ? const HomeScreen() : const FirstScreen(),
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
         primaryColor: const Color(0xFFE9435A),
