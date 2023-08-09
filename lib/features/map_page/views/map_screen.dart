545ddc13ab8f6779020b00acbfadd73b695356eb
jinki <jkhsky121@gmail.com>
2023-08-09 21:24:26 +0900

    marker 탭하면 요약 정보 표시

--- lib/features/map_page/views/map_screen.dart
+++ lib/features/map_page/views/map_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -6,7 +7,9 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../../../constants/gaps.dart';
 import '../../../constants/sizes.dart';
+import '../../detailpage/models/cafe_model.dart';
 import '../../detailpage/view_models/cafe_vm.dart';
+import '../../detailpage/views/detailScreen.dart';
 import '../../home/views/search_screen.dart';
 
 class MapScreen extends ConsumerStatefulWidget {
@@ -32,7 +35,19 @@ class _MapScreenState extends ConsumerState<MapScreen> {
     );
   }
 
+  void _onDetailTap() {
+  Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => DetailScreen(
+          cafe: _selectedCafe!,
+        ),
+      ),
+    );
+  }
+
   Set<Marker> _markers = {};
+
+  Cafe? _selectedCafe;
 
   void _loadMarkers() async {
     final cafes = await ref.read(cafesProvider.future);
@@ -43,10 +58,14 @@ class _MapScreenState extends ConsumerState<MapScreen> {
               (cafe) => Marker(
                 markerId: MarkerId(cafe.name),
                 position: LatLng(cafe.lat, cafe.lng),
+                onTap: () {
+                  print("InfoWindow tapped ${cafe.name}");
+                  setState(() {
+                    _selectedCafe = cafe;
+                  });
+                },
                 infoWindow: InfoWindow(
                   title: cafe.name,
-                  snippet: cafe.address,
-                  onTap: () {},
                 ),
               ),
             )
@@ -106,13 +125,90 @@ class _MapScreenState extends ConsumerState<MapScreen> {
           ),
         ),
       ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        markers: _markers,
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 11.0,
-        ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            markers: _markers,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 11.0,
+            ),
+            onTap: (position) {
+              setState(() {
+                _selectedCafe = null;
+              });
+            },
+          ),
+          if (_selectedCafe != null)
+            Positioned(
+              bottom: Sizes.size80,
+              left: 0,
+              right: 0,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: Sizes.size28),
+                child: GestureDetector(
+                  onTap: _onDetailTap,
+                  child: Container(
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(Sizes.size12),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.8),
+                          spreadRadius: 1,
+                          blurRadius: Sizes.size8,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    height: 110,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 3,
+                          child: CachedNetworkImage(
+                            height: double.infinity,
+                            fit: BoxFit.cover,
+                            imageUrl: _selectedCafe!.imageUri,
+                            errorWidget: (context, url, error) =>
+                                const Icon(Icons.error),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 7,
+                          child: Padding(
+                            padding: const EdgeInsets.all(Sizes.size14),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(_selectedCafe!.name, style: const TextStyle(fontWeight: FontWeight.w600),),
+                                    const FaIcon(
+                                      FontAwesomeIcons.solidHeart,
+                                      size: Sizes.size16,
+                                      color: Colors.black,
+                                    ),
+                                  ],
+                                ),
+                                Gaps.v20,
+                                Text('${_selectedCafe!.lat}, ${_selectedCafe!.lng}'),
+                                Text('${_selectedCafe!.openingTime} ~ ${_selectedCafe!.closingTime}'),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
