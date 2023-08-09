5dbef057fa7f1ae6baa9f79288c31e3e91d52005
jinki <jkhsky121@gmail.com>
2023-08-09 15:03:59 +0900

    GoogleMap에 markers 표시

--- lib/features/map_page/views/map_screen.dart
+++ lib/features/map_page/views/map_screen.dart
@@ -1,24 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
 import '../../../constants/gaps.dart';
 import '../../../constants/sizes.dart';
+import '../../detailpage/view_models/cafe_vm.dart';
 import '../../home/views/search_screen.dart';
 
-class MapScreen extends StatefulWidget {
+class MapScreen extends ConsumerStatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
 
   @override
-  _MapScreenState createState() => _MapScreenState();
+  ConsumerState createState() => _MapScreenState();
 }
 
-class _MapScreenState extends State<MapScreen> {
+class _MapScreenState extends ConsumerState<MapScreen> {
   late GoogleMapController _mapController;
 
-  final LatLng _center = const LatLng(35.8714, 128.6014);
+  final LatLng _center = const LatLng(37.3433, 127.9170);
 
   void _onMapCreated(GoogleMapController controller) {
     _mapController = controller;
@@ -31,58 +32,88 @@ class _MapScreenState extends State<MapScreen> {
     );
   }
 
+  Set<Marker> _markers = {};
+
+  void _loadMarkers() async {
+    final cafes = await ref.read(cafesProvider.future);
+    setState(
+      () {
+        _markers = cafes
+            .map(
+              (cafe) => Marker(
+                markerId: MarkerId(cafe.name),
+                position: LatLng(cafe.lat, cafe.lng),
+                infoWindow: InfoWindow(
+                  title: cafe.name,
+                  snippet: cafe.address,
+                  onTap: () {},
+                ),
+              ),
+            )
+            .toSet();
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMarkers();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Padding(
-            padding: const EdgeInsets.only(
-              right: Sizes.size14,
-            ),
-            child: Row(
-              children: [
-                Expanded(
-                  child: GestureDetector(
-                    onTap: () => _onSearchBarTap(context),
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: Sizes.size14,
-                        vertical: Sizes.size8,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(Sizes.size20),
-                      ),
-                      child: const Row(
-                        children: [
-                          FaIcon(
-                            FontAwesomeIcons.magnifyingGlass,
-                            size: Sizes.size16,
+      appBar: AppBar(
+        title: Padding(
+          padding: const EdgeInsets.only(
+            right: Sizes.size14,
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onTap: () => _onSearchBarTap(context),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: Sizes.size14,
+                      vertical: Sizes.size8,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                      borderRadius: BorderRadius.circular(Sizes.size20),
+                    ),
+                    child: const Row(
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.magnifyingGlass,
+                          size: Sizes.size16,
+                        ),
+                        Gaps.h5,
+                        Text(
+                          '검색',
+                          style: TextStyle(
+                            fontSize: Sizes.size20,
+                            color: Colors.grey,
                           ),
-                          Gaps.h5,
-                          Text(
-                            '검색',
-                            style: TextStyle(
-                              fontSize: Sizes.size20,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        markers: _markers,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
         ),
+      ),
     );
-}
+  }
 }
