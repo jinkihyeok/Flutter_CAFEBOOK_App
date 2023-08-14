d5de8484cdc7750d6f9aafe4471050bd7390ecc2
jinki <jkhsky121@gmail.com>
2023-08-14 15:33:58 +0900

    favor_screen : favorites 데이터만 필터링하기

--- lib/features/home/views/favor_screen.dart
+++ lib/features/home/views/favor_screen.dart
@@ -1,22 +1,36 @@
+import 'package:caffe_app/features/detailpage/view_models/cafe_vm.dart';
+import 'package:caffe_app/features/home/view_models/user_vm.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavorScreen extends StatefulWidget {
+class FavorScreen extends ConsumerWidget {
   const FavorScreen({Key? key}) : super(key: key);
 
   @override
-  FavorScreenState createState() => FavorScreenState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userProfile = ref.watch(usersProvider).value;
+    final cafeData = ref.watch(cafesProvider).value;
+    final favorites = cafeData
+            ?.where((cafe) => userProfile?.favorites.contains(cafe.id) ?? false)
+            .toList() ??
+        [];
 
-class FavorScreenState extends State<FavorScreen> {
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('즐겨찾기'),
       ),
-      body: const Center(
-        child: Text('Favor'),
-      ),);
+      body: userProfile == null || cafeData == null
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: favorites.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(favorites[index].name),
+                );
+              }),
+    );
   }
 }
