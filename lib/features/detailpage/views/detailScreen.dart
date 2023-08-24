0fa60fbe34d314fa857041e2484736c2127fd11b
jinki <jkhsky121@gmail.com>
2023-08-24 15:41:49 +0900

    detail_screen 수정 : UI 수정

--- lib/features/detailpage/views/detailScreen.dart
+++ lib/features/detailpage/views/detailScreen.dart
@@ -1,11 +1,11 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:caffe_app/constants/gaps.dart';
 import 'package:caffe_app/features/home/view_models/user_vm.dart';
+import 'package:caffe_app/util/distances_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../../../constants/sizes.dart';
-import '../../../util/calculate_distances.dart';
 import '../models/cafe_model.dart';
 
 class DetailScreen extends ConsumerStatefulWidget {
@@ -29,7 +29,8 @@ class _DetailScreenState extends ConsumerState<DetailScreen> {
   void _setupFavoriteStatus() async {
     final cafeId = widget.cafe.id;
 
-    List<String> userFavorites = await ref.read(usersProvider.notifier).getUserFavorites();
+    List<String> userFavorites =
+        await ref.read(usersProvider.notifier).getUserFavorites();
     setState(() {
       isFavorite = userFavorites.contains(cafeId);
     });
@@ -53,6 +54,11 @@ class _DetailScreenState extends ConsumerState<DetailScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text(
+          widget.cafe.name,
+          style: const TextStyle(
+              fontSize: Sizes.size20, fontWeight: FontWeight.w600),
+        ),
         actions: [
           Padding(
             padding: const EdgeInsets.symmetric(
@@ -83,31 +89,254 @@ class _DetailScreenState extends ConsumerState<DetailScreen> {
             ),
           ),
           Gaps.v20,
-          Text(widget.cafe.name),
-          Text(widget.cafe.location),
-          Text(widget.cafe.address),
-          FutureBuilder<int?>(
-            future: calculateDistances(ref, widget.cafe),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const SizedBox(
-                  width: Sizes.size12,
-                  height: Sizes.size12,
-                  child: CircularProgressIndicator(
-                    color: Colors.black,
-                    strokeWidth: 2,
-                  ),
-                );
-              } else if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              } else if (snapshot.hasData && snapshot.data != null) {
-                return Text('${snapshot.data} km');
-              } else {
-                return const SizedBox.shrink();
-              }
-            },
+          DefaultTextStyle(
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: Sizes.size16,
+            ),
+            child: Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(Sizes.size20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        const FaIcon(FontAwesomeIcons.locationDot),
+                        Gaps.h10,
+                        Text(widget.cafe.location),
+                        const SizedBox(
+                          height: 25,
+                          child: VerticalDivider(
+                            color: Colors.grey,
+                            thickness: 1,
+                            indent: 5,
+                            endIndent: 5,
+                          ),
+                        ),
+                        DistanceWidget(cafe: widget.cafe),
+                        const SizedBox(
+                          height: 25,
+                          child: VerticalDivider(
+                            color: Colors.grey,
+                            thickness: 1,
+                            indent: 5,
+                            endIndent: 5,
+                          ),
+                        ),
+                        Text(widget.cafe.address),
+                      ],
+                    ),
+                    Gaps.v16,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        const FaIcon(FontAwesomeIcons.clock),
+                        Gaps.h10,
+                        Text(
+                          '${widget.cafe.openingTime} ~ ${widget.cafe.closingTime}',
+                        ),
+                        const SizedBox(
+                          height: 25,
+                          child: VerticalDivider(
+                            color: Colors.grey,
+                            thickness: 1,
+                            indent: 5,
+                            endIndent: 5,
+                          ),
+                        ),
+                        const Text(
+                          '매주 월요일 휴무',
+                        ),
+                      ],
+                    ),
+                    Gaps.v40,
+                    Padding(
+                      padding:
+                          const EdgeInsets.symmetric(horizontal: Sizes.size40),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Expanded(
+                            child: Container(
+                              height: 1,
+                              color: Colors.black.withOpacity(0.5),
+                            ),
+                          ),
+                          const Padding(
+                            padding:
+                                EdgeInsets.symmetric(horizontal: Sizes.size16),
+                            child: Text(
+                              "I N F O",
+                              style: TextStyle(fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              height: 1,
+                              color: Colors.black.withOpacity(0.5),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Gaps.v24,
+                    Padding(
+                      padding:
+                          const EdgeInsets.symmetric(horizontal: Sizes.size40),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                              clipBehavior: Clip.antiAlias,
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.circular(Sizes.size12),
+                              ),
+                              child: Image.asset(
+                                'assets/icons/logo_naver.png',
+                                scale: 0.6,
+                              ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                              clipBehavior: Clip.antiAlias,
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.circular(Sizes.size12),
+                              ),
+                              child: Image.asset(
+                                'assets/icons/logo_daum.png',
+                                scale: 0.6,
+                              ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                              clipBehavior: Clip.antiAlias,
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.circular(Sizes.size12),
+                              ),
+                              child: Image.asset(
+                                'assets/icons/google.png',
+                                scale: 0.6,
+                              ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                              clipBehavior: Clip.antiAlias,
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.circular(Sizes.size12),
+                              ),
+                              child: Image.asset(
+                                'assets/icons/logo_instagram.png',
+                                scale: 0.6,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Gaps.v48,
+                    Padding(
+                      padding:
+                          const EdgeInsets.symmetric(horizontal: Sizes.size40),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Expanded(
+                            child: Container(
+                              height: 1,
+                              color: Colors.black.withOpacity(0.5),
+                            ),
+                          ),
+                          const Padding(
+                            padding:
+                                EdgeInsets.symmetric(horizontal: Sizes.size16),
+                            child: Text(
+                              "M A P",
+                              style: TextStyle(fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              height: 1,
+                              color: Colors.black.withOpacity(0.5),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Gaps.v24,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: Sizes.size40),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                              clipBehavior: Clip.antiAlias,
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.circular(Sizes.size12),
+                              ),
+                              child: Image.asset(
+                                'assets/icons/logo_naver_map.png',
+                                scale: 0.6,
+                              ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                              clipBehavior: Clip.antiAlias,
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.circular(Sizes.size12),
+                              ),
+                              child: Image.asset(
+                                'assets/icons/logo_kakao_map.png',
+                                scale: 0.6,
+                              ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                              clipBehavior: Clip.antiAlias,
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.circular(Sizes.size12),
+                              ),
+                              child: Image.asset(
+                                'assets/icons/logo_google_map.png',
+                                scale: 0.6,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
           ),
-          Text('${widget.cafe.openingTime} ~ ${widget.cafe.closingTime}'),
         ],
       ),
     );
